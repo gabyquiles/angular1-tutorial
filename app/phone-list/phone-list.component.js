c4753118a8a8ddfe036a8558cae47d4a07241a6d
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-07-29 16:49:30 -0400

    Step 7: Get data from "web"

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -6,22 +6,13 @@ module('phoneList').
 component('phoneList', {
     // Note: The URL is relative to our `index.html` file
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-        this.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.',
-                age: 1
-            }, {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 2
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 3
-            }
-        ];
-        this.orderProp = 'age';
-    }
+    controller: ['$http',
+        function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+
+        $http.get('phones/phones.json').then(function (response) {
+            self.phones = response.data;
+        })
+    }]
 });
