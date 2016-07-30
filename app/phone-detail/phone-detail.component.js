5692c8ccd73067e9c116632ddba54ce9554c9749
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-07-30 16:11:32 -0400

    Step 12: Add event handler to select image

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -6,8 +6,13 @@ component('phoneDetail', {
         function PhoneDetailController($http, $routeParams) {
             var self = this;
 
+            self.setImage = function (imageUrl) {
+                self.mainImageUrl = imageUrl;
+            };
+
             $http.get("phones/"+$routeParams.phoneId + ".json").then(function (response) {
                 self.phone = response.data;
+                self.setImage(self.phone.images[0]);
             })
         }
     ]
